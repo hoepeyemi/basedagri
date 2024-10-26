66d6cce10d7b3bda01189b1a262af96fc5bc23e2
Afoxcute A <Jwavolabilove16@gmail.com>
2024-10-26 15:11:17 +0000

    hero img fixed

--- frontend/src/components/homepage_components/HeroSection.jsx
+++ frontend/src/components/homepage_components/HeroSection.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
     <>
       <section className="container mx-auto h-full border-t-2 mt-20 bg-white font-montserrat">
         <div className="mt-10 md:mt-20 pb-20 mx-auto bg-herobg lg:bg-contain bg-no-repeat sm:bg-right">
-          <div className=" flex flex-col lg:flex-row w-[90%] max-w-[90%] mx-auto justify-start items-center">
-            <div className=" flex flex-col lg:w-[50%] text-center items-center lg:items-start lg:text-left">
-              <h2 className=" text-3xl md:text-[45px] font-[800] md:leading-[90px] tracking-[1px]">
+          <div className="flex flex-col lg:flex-row w-[90%] max-w-[90%] mx-auto justify-between items-center">
+            <div className="flex flex-col lg:w-[60%] text-center items-center lg:items-start lg:text-left">
+              <h2 className="text-3xl md:text-[45px] font-[800] md:leading-[90px] tracking-[1px]">
                 BASEARPSAGRIC
               </h2>
-              <h2 className=" text-4xl md:text-[60px] tracking-[1px] font-black text-[#000000]">
+              <h2 className="text-4xl md:text-[60px] tracking-[1px] font-black text-[#000000]">
                 FARMS
               </h2>
               <p className="w-[275px] font-bold text-base md:text-[20px] tracking-[-0.5px] leading-[25px] mt-6">
@@ -30,8 +30,12 @@ const Hero = () => {
               <MediumButton text={"Learn more"} linkto={"/about-us"} />
             </div>
 
-            <div className="lg:w-[50%]">
-              <img src={heroImage} alt="home-img" className="lg:w-full xl:-w-[993px] mt-2" />
+            <div className="lg:w-[35%] ml-auto">
+              <img 
+                src={heroImage} 
+                alt="home-img" 
+                className="w-full object-contain max-w-md ml-auto" 
+              />
             </div>
           </div>
         </div>
